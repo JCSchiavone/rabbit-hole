8953e33e232e136f92e9ae40c6f7243b35472072
Amy Zhang <axz@Amys-MacBook-Pro.local>
2016-01-09 16:56:03 -0500

    process stuff

---
+++ alice_dummy.js
@@ -0,0 +1,22 @@
+var data = [  
+   {  
+      "text":"I. DOWN THE RABBIT-HOLE",
+      "loc": {  
+         "x":0,
+         "y":200,
+      },
+      "font": "40px Arial",
+      "color": "#000000",
+      "font_size": 60,
+   },
+   {  
+      "text":"Alice was beginning to get very tired",
+      "loc": {  
+         "x":350,
+         "y":400,
+      },
+      "font": "20px Garamond",
+      "color": "#000000",
+      "font_size": 30,
+   }
+]
