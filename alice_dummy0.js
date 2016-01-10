09bf04ec062809c701dd9d27cbe7540c15081bd1
Amy Zhang <axz@Amys-MacBook-Pro.local>
2016-01-09 20:04:02 -0500

    second step

---
+++ alice_dummy0.js
@@ -0,0 +1,120 @@
+data = [{
+	"text": "I--DOWN THE RABBIT-HOLE",  
+	"font": "50px Cinzel Decorative"
+}, 
+{
+	"text": "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do.",
+	"font": "24px Josefin Slab"
+}, 
+{
+	"text": "Oh dear!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "I shall be too late!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "In another moment, down went Alice after it!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Either the well was very deep, or she fell very slowly, for she had plenty of time, as she went down, to look about her.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "She took down a jar from one of the shelves as she passed.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Down, down, down!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Would the fall never come to an end?",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "There was nothing else to do, so Alice soon began talking to herself.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "\"Dinah'll miss me very much to-night, I should think!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "(Dinah was the cat.)",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "\"I hope they'll remember her saucer of milk at tea-time.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Dinah, my dear, I wish you were down here with me!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Alice felt that she was dozing off, when suddenly, thump!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "thump!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "down she came upon a heap of sticks and dry leaves, and the fall was over.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Alice was not a bit hurt, and she jumped up in a moment.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "There was not a moment to be lost.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "She was close behind it when she turned the corner, but the Rabbit was no longer to be seen.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "She found herself in a long, low hall, which was lit up by a row of lamps hanging from the roof.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Suddenly she came upon a little table, all made of solid glass.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "*       *       *       *       *  \"What a curious feeling!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "said Alice.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "\"I must be shutting up like a telescope!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "And so it was indeed!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "\"Come, there's no use in crying like that!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "said Alice to herself rather sharply.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "\"I advise you to leave off this minute!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "She ate a little bit and said anxiously to herself, \"Which way?",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Which way?\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "So she set to work and very soon finished off the cake.",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "II--THE POOL OF TEARS   \"Curiouser and curiouser!\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "cried Alice (she was so much surprised that for the moment she quite forgot how to speak good English).",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "\"Now I'm opening out like the largest telescope that ever was!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Good-by, feet!",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "Oh, my poor little feet, I wonder who will put on your shoes and stockings for you now, dears?",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "I shall be a great deal too far off to trouble myself about you.\"",
+	"font": "24px Josefin Slab"
+}, {
+	"text": "She sat down and began to cry again.",
+	"font": "24px Josefin Slab"
+}]
